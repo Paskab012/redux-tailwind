7c4e0c10c9a11d45946976f47691f4be3422fa6d
KABIKA681 <pascalkabikampan@gmail.com>
2022-04-04 14:44:13 +0200

    Correct UI

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -10,15 +10,15 @@ function Navbar() {
         <NavLink exact to="/NewSubmission" activeClassName="nav-active"> New Submission</NavLink>
       </div> */}
 
-      <nav className="bg-gray-800">
+      <nav className="bg-gray-200">
         <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative flex items-center justify-between h-16">
             <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
               <div className="hidden sm:block sm:ml-6">
                 <div className="flex space-x-4">
-                  <NavLink to="/NewSubmission" className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">New Submission</NavLink>
+                  <NavLink to="/NewSubmission" className="px-3 py-2 text-sm font-bold text-red-400 rounded-md hover:bg-gray-700 hover:text-white">New Submission</NavLink>
 
-                  <NavLink to="/AllSubmission" className="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">All Submission</NavLink>
+                  <NavLink to="/AllSubmission" className="px-3 py-2 text-sm font-bold text-blue-400 rounded-md hover:bg-gray-700 hover:text-white">All Submission</NavLink>
 
                 </div>
               </div>
